5be1495f535262e6b903afb51d9053d00ecc53b2
machoneywell <mhwithmt@gmail.com>
2026-01-11 02:18:01 -0600

    Add files via upload

---
+++ Animated/Animated/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Animated
+{
+    public partial class Animated : Form
+    {
+        public Animated()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            label.Text = "Monarch Butterflies start migrating South in September";
+            label.Location = new Point(130, 390);
+        }
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            label.Text = "Adult Monarch Butterflies live for 4-6 weeks";
+            label.Location = new Point(185, 390);
+        }
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            label.Text = "This is a Monarch Butterfly";
+            label.Location = new Point(265, 390);
+        }
+    }
+}
